7bec77d9a873a33c0742592e331c9e81807c05ea
bipulkr-java <singharjundev2@gmail.com>
2024-11-29 16:28:50 +0530

    Initial Changes

---
+++ SpringDockerProj02/src/main/java/com/bk/SpringDockerProj02Application.java
@@ -0,0 +1,16 @@
+package com.bk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerProj02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerProj02Application.class, args);
+		
+		System.out.println("You are running a SpringBoot Application ");
+		
+	}
+
+}
